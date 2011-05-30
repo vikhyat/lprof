889d17817edc952935ca1386ac2041a47e29f10d
Vikhyat Korrapati <vikhyatk@gmail.com>
2011-05-30 13:20:06 +0530

    readability enhancements

--- lprof.rb
+++ lprof.rb
@@ -26,28 +26,30 @@ if variable[0] != "$"
   ferror "counting variable should begin with $: '#{variable}'"
 end
 
+# create a temporary file to store annotated code
 out = File.new(filename+ext, 'w')
+out.puts "#{variable} = Array.new(#{range[1]-range[0]+1}) { 0 }"
 
-out.puts("#{variable} = Array.new(#{range[1]-range[0]+1}) { 0 }")
+# write all the code to the temporary file, annotating where necessary
 0.upto(code.length-1) do |line_number|
   if line_number >= range[0] and line_number <= range[1]
+    # increment the counter for this line
     out.puts "#{variable}[#{line_number-range[0]}] += 1"
   end
+  # execute this line
   out.puts code[line_number]
 end
 
-footer = [
-  "$stderr.puts(' --------------------------')",
-  "$stderr.puts(' LINE-WISE PROFILING REPORT')",
-  "$stderr.puts(' --------------------------')",
-  "ml = #{variable}.max.to_s.length"
-]
+out.puts "$stderr.puts(' --------------------------')"
+out.puts "$stderr.puts(' LINE-WISE PROFILING REPORT')"
+out.puts "$stderr.puts(' --------------------------')"
+out.puts "ml = #{variable}.max.to_s.length"
+
 range[0].upto(range[1]) do |line_number|
-  footer << "$stderr.printf \"%\#{ml}d | \", #{variable}[#{line_number-range[0]}]"
-  footer << "$stderr.puts '#{code[line_number].chomp}'"
+  out.puts "$stderr.printf \"%\#{ml}d | \",#{variable}[#{line_number-range[0]}]"
+  out.puts "$stderr.puts '#{code[line_number].chomp}'"
 end
 
-out.puts footer
 out.close
 
 print `echo '#{input}' | ruby #{filename}#{ext} > /dev/null`
